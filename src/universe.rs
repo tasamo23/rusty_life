3f031e64bdc76331261b84b8812c1b28ca43c584
Masato Arnold <github.stage032@passinbox.com>
2024-07-23 20:40:03 +0200

    Fix bugs, working

--- src/universe.rs
+++ src/universe.rs
@@ -1,4 +1,10 @@
+use js_sys::Math::random;
+use renderer::create_canvas;
+use renderer::Renderer;
+
 use crate::wasm_bindgen;
+
+pub mod renderer;
 
 use crate::Cell;
 
@@ -7,9 +13,14 @@ pub struct Universe {
     width: u32,
     height: u32,
     cells: Vec<Cell>,
+    renderer: Renderer,
 }
 
 impl Universe {
+    pub fn cell_at_index(&self, x: u32, y: u32) -> Cell {
+        self.cells[self.get_index(x, y)]
+    }
+
     fn get_index(&self, x: u32, y: u32) -> usize {
         (y * self.width + x) as usize
     }
@@ -19,11 +30,10 @@ impl Universe {
 
         for offset_x in [self.width - 1, 0, 1].iter().cloned() {
             for offset_y in [self.height - 1, 0, 1].iter().cloned() {
-                let index_x = (offset_x + x) % self.width;
-                let index_y = (offset_y + y) % self.height;
-
-                if index_x != 0 && index_y != 0 {
-                    count += self.cells[self.get_index(index_x, index_y)] as u8;
+                if (offset_x != 0) || (offset_y != 0) {
+                    let index_x = (offset_x + x) % self.width;
+                    let index_y = (offset_y + y) % self.height;
+                    count += self.cell_at_index(index_x, index_y) as u8;
                 }
             }
         }
@@ -38,8 +48,7 @@ impl Universe {
 
         for x in 0..self.width {
             for y in 0..self.height {
-                let index = self.get_index(x, y);
-                let cell = self.cells[index];
+                let cell = self.cell_at_index(x, y);
 
                 let live_neighbors = self.live_neighbor_count(x, y);
 
@@ -47,10 +56,11 @@ impl Universe {
                     (Cell::Alive, x) if x < 2 => Cell::Dead,
                     (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
                     (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
                     (otherwise, _) => otherwise,
                 };
 
-                next_cells[index] = next_cell_state;
+                next_cells[self.get_index(x, y)] = next_cell_state;
             }
         }
 
@@ -59,32 +69,30 @@ impl Universe {
 
     pub fn new(width: u32, height: u32) -> Universe {
         let cells = (0..width * height)
-            .map(|i| {
-                if i % 7 == 0 || i % 3 == 0 {
+            .map(|_| {
+                if random() > 0.7 {
                     Cell::Alive
                 } else {
                     Cell::Dead
                 }
             })
             .collect::<Vec<Cell>>();
+
+        let scale = 4;
+
+        let canvas = create_canvas(width, height, scale);
+
+        let renderer = Renderer::new(canvas, scale);
+
         Universe {
             width,
             height,
             cells,
+            renderer,
         }
     }
 
-    pub fn render(&self) {}
-
-    pub fn width(&self) -> u32 {
-        self.width
-    }
-
-    pub fn height(&self) -> u32 {
-        self.height
-    }
-
-    pub fn cells(&self) -> *const Cell {
-        self.cells.as_ptr()
+    pub fn render(&self) {
+        self.renderer.draw_cells(self);
     }
 }
